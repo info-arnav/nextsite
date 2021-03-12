e59ccb9810dbb7ef80fc901abf54ecef120e5a81
info-arnav <info@arnavgupta.net>
2021-03-12 19:57:23 +0530

    lang

--- next.config.js
+++ next.config.js
@@ -3,4 +3,20 @@ module.exports = {
     mongodburl:
       "mongodb+srv://arnav:Arnav300804@cluster0.qzvuc.mongodb.net/dataset?retryWrites=true&w=majority",
   },
+  i18n: {
+    locales: ["en-US"],
+    defaultLocale: "en-US",
+    domains: [
+      {
+        domain: "arnavgupta.net",
+        defaultLocale: "en-US",
+      },
+    ],
+    domains: [
+      {
+        domain: "passionatebloggers.me",
+        defaultLocale: "en-US",
+      },
+    ],
+  },
 };
