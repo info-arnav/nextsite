8b65031f1297e5612d2bd4aa9bd004ac2542dff9
info-arnav <info@arnavgupta.net>
2021-03-13 15:52:00 +0530

    prebuild3

--- pages/index.js
+++ pages/index.js
@@ -828,7 +828,7 @@ export default function Home({ res }) {
 export async function getStaticProps() {
   // Call an external API endpoint to get posts.
   // You can use any data fetching library
-  const res = await fetch("/api/homeData");
+  const res = await fetch("https://www.arnavgupta.net/api/homeData");
 
   // By returning { props: posts }, the Blog component
   // will receive `posts` as a prop at build time
